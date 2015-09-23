79dc8247c363c1c22c1636caa6e6c6e6eed55783
victorsolonets <victor.solonets@gmail.com>
2015-09-23 16:49:53 +0300

    create first version of application.

---
+++ app/src/main/java/com/example/marcusedition/pddproject/otherActivity/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.example.marcusedition.pddproject.otherActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by victor on 23.09.15.
+ */
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+//        setContentView(R.layout.menu_activity);
+    }
+}
